7fe0bd4257007bd82baff145a0b6bcdeaf6b0937
Lenny Zeitoun <zeitounlenny@gmail.com>
2021-01-06 11:39:00 +0200

    ➕ Added some debugging functions for the monitoring sys

--- tasks/logs_monitoring.js
+++ tasks/logs_monitoring.js
@@ -3,11 +3,19 @@ const axios = require('axios').default;
 const NginxConfFile = require('nginx-conf').NginxConfFile;
 
 // Create a watcher for logfile
-const follower = follow('/var/log/syslog');
+const follower = follow('/var/log/syslog', { persistent: true });
 // Errors we want to receive an alert for
 const error_logs_lvls = ['ERROR', 'CRIT', 'ALERT', 'EMERG'];
 // Slack details
 const slack_webhook_endpoint = process.env.SLACK_WEBHOOK || "YOUR_SLACK_WEBHOOK";
+
+// Added some debug functions for the follower
+follower.on('close', function () {
+  console.log("We must have called follower.close()");
+});
+follower.on('error', function (event, filename, value) {
+  console.log("Oh noes! Here's the error message: " + value);
+});
 
 NginxConfFile.create('/etc/nginx/sites-available/bigbluebutton', async (err, conf) => {
   if (err) {
